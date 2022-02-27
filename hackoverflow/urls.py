2ab36c4fdeda436d284e1cc01c12e23e222da7d4
Cooper Sheets <coopersheets@gmail.com>
2022-02-27 13:08:52 -0600

    Basic view config set up

--- hackoverflow/urls.py
+++ hackoverflow/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('messaging.urls')),
+    path('store/', include('store.urls')),
 
     path('admin/', admin.site.urls),
 ]
