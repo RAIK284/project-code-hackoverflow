5f55144501851192363f43d955b6e1bd9f311eb8
wa3401 <69916271+wa3401@users.noreply.github.com>
2022-03-29 23:35:01 -0500

    Messing w/ Boostrap

--- messaging/views.py
+++ messaging/views.py
@@ -67,11 +67,26 @@ def register_user_page(request):
     context = {'form': form}
     return render(request, 'messaging/login_register.html', context)
 
+    
+@login_required(login_url='login')
 def inbox(request):
     """View for the user's inbox."""
     convos = Conversation.objects.filter(userGroup__members__in=[request.user.id])
     
-    context = {'convos': convos}
+    names = []
+    for convo in convos:
+        first_message = Message.objects.filter(conversation=convo)[0]
+        username_list = convo.name.split('-')
+        username_list.remove(request.user.username)
+        name_list = []
+        for username in username_list:
+            name_list.append(User.objects.get(username=username).get_full_name())
+        name = ', '.join(name_list)
+        names.append([convo, name, first_message])
+
+    user_name = request.user.get_full_name()
+    
+    context = {'names':names, 'user_name':user_name}
     return render(request, 'messaging/inbox.html', context)
 
 def send_message(request):
@@ -138,7 +153,7 @@ def create_convo(request):
         for username in send_to_list:
             user_group.members.add(User.objects.get(username=username))
 
-        convo = Conversation.objects.create(name=group_name, user_group=user_group)
+        convo = Conversation.objects.create(name=group_name, userGroup=user_group)
         convo.save()
         
         return user_group, convo
